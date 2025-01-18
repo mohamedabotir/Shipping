df2b2b903e2fcfaa64dd47a645d6fc3ded8396f8
mohamedabotir <mohamedabotyear@gmail.com>
2025-01-18 03:16:00 +0200

    add remaining files

---
+++ ShippingOrderDb/ShippingOrder/Tables/ShippingOrder.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [ShippingOrder] (
+    OrderId INT IDENTITY(1,1) PRIMARY KEY, 
+    Guid UNIQUEIDENTIFIER NOT NULL,       
+    CustomerName NVARCHAR(50) NOT NULL, 
+    CustomerAddress NVARCHAR(256) NOT NULL, 
+    CustomerPhoneNumber NVARCHAR(15) NOT NULL, 
+    TotalAmount DECIMAL(18,2) NOT NULL,  
+    IsActive INT NOT NULL,              
+    PurchaseOrderNumber NVARCHAR(25) NOT NULL, 
+    OrderStage INT NOT NULL             
+);
