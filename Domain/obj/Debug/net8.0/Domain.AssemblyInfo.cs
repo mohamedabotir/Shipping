799e595aec7542625d3730df74743751ddc4159d
mohamedabotir <mohamedabotyear@gmail.com>
2025-01-18 20:02:23 +0200

    subscribe closed event

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474f8bfb3caee997dff7c2bc374d9c53ae2189ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d2d6a26d5d2e369d03f91e85ec21f477b92588")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
